24a88adf08f57fdf2f982af864c300690636fd4c
Pradnya Borkar <pradnyaborkar90@gmail.com>
2018-06-22 17:09:05 +0530

    Update sentimentAnalysis.py

--- sentimentAnalysis.py
+++ sentimentAnalysis.py
@@ -12,10 +12,10 @@ text.pack(expand="yes", fill='both')
 f = Figure(figsize=(5,5), dpi=100)
 a = f.add_subplot(111)
 """
-Consumer_Key='55HfunDO3D2ZtNf45h10fpI7N'
-Consumer_Secret='RMOStDyzUcLAbqi2lIeMJeIplRRrUhnehOjhTHxpY4ge3TFH4R'
-Access_Token='938251354602061825-n5zubgcEwgmPehVB74JbRmsLLyGvY76'
-Access_Token_Secret='rkbXDA3jiMyJABdZYZxolHp3eD9YJ7DvPzMfCdZ9hKGPY'
+Consumer_Key='XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+Consumer_Secret='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+Access_Token='XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+Access_Token_Secret='XXXXXXXXXXXXXXXXXXXXXXXXXXX'
 def submit():
 	tweets_list.clear()
 	labels= 'Negative', 'Positive', 'Neutral'
